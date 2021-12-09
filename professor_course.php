a5b6210ebf7e892467fcf0524fbfab8e7adeffaa
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-08 19:25:32 -0800

    Create professor_course.php

---
+++ professor_course.php
@@ -0,0 +1,8 @@
+<html>
+    <?php
+    $link = mysqli_connect('mariadb', 'cs332t14', '10141854u2y4e2b', 'cs332t14');
+    if (!link) {
+        die("ERROR. Unable to connect: ".mysqli_connect_error());
+    }
+    ?>
+</html>
