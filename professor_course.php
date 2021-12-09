16273ab570362e86a4dae4d6c66fa7950482add3
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-09 13:08:57 -0800

    Update professor_course.php

--- professor_course.php
+++ professor_course.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect('mariadb', 'cs332t14', '10141854u2y4e2b', 'cs332t14');
+    $link = mysqli_connect('mariadb', 'cs332t14', '6nXl2UmX', 'cs332t14');
     
     if (!$link) {
         die("ERROR. Unable to connect. ".mysqli_error());
@@ -9,18 +9,15 @@
 
     $SSN = $_POST["SSN"];
 
-    if ($SSN != "") {
-        $query = "SELECT Course_Title, Classroom, Meeting_Days, Beg_Time, End_Time
+
+    $query = "SELECT Course_Title, Classroom, Meeting_Days, Beg_Time, End_Time
     FROM COURSE_SECTION as cs, COURSE as c
     WHERE cs.P_SSN = '$SSN' AND c.Course_Num = cs.Course_Num;";
-    }
-
-    echo $query;
 
     $result = $link->query($query);
 
-    if($result->num_rows > 0) {
-        echo "SSN: ".$SSN."<br>";
+    if($result) {
+        echo "SSN: -".$SSN."<br>";
         while($row = $result->fetch_assoc()) {
             echo "Title: ".$row["Course_Title"]."  Classroom: ".$row["Classroom"]."  Beginning Time: ".$row["Beg_Time"]."End".$row[End_Time];
         }
