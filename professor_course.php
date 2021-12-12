9dab84c4115386286d9cb0f3a5c844edf4bd07f6
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-11 22:28:20 -0800

    Update professor_course.php

--- professor_course.php
+++ professor_course.php
@@ -14,12 +14,16 @@
     FROM COURSE_SECTION as cs, COURSE as c
     WHERE cs.P_SSN = '$SSN' AND c.Course_Num = cs.Course_Num;";
 
+    /* try doing this
+    $query = "SELECT * FROM COURSE;" */
+
     $result = $link->query($query);
 
+    /*Try using one apostrophe for variables inside the brackets.*/
     if($result) {
         echo "SSN: -".$SSN."<br>";
         while($row = $result->fetch_assoc()) {
-            echo "Title: ".$row["Course_Title"]."  Classroom: ".$row["Classroom"]."  Beginning Time: ".$row["Beg_Time"]."End".$row[End_Time];
+            echo "Title: ".$row["Course_Title"]."  Classroom: ".$row["Classroom"]."  Beginning Time: ".$row["Beg_Time"]."End Time: ".$row['End_Time'];
         }
     }
     else {
