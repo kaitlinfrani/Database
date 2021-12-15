984c75d3b74bd32d5d00dd259bfc406c335961b1
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-15 00:24:48 -0800

    Query needs to be fixed

--- professor_grade.php
+++ professor_grade.php
@@ -1,27 +1,24 @@
 <html>
     <body>
         <?php
-        
         $link = mysqli_connect('mariadb', 'cs332t14', '6nXl2UmX','cs332t14');
         if (!$link) {
             die('Could not connect: ' . mysqli_error());
         }
 
-        else {
-            echo "Connected Successfully.<br><br>";
-        }
+        $cnum = $_POST["coursenum"];
+        $snum = $_POST["sectionnum"];
+        
+        $query = "SELECT Student_Grade, COUNT(*)
+        FROM COURSE_SECTION as s, COURSE as c, ENROLLMENT as e
+        WHERE c.Course_Num = '".$cnum."' AND s.Section_Num = '".$sum."' AND c.Course_Num = s.Course_Num AND s.Course_Num = e.Course_Num
+        AND s.Section_Num = e.Section_Num;";
+        
+        $result = $link->query($query);
 
-        if(isset($_POST['sectionnum'])) {
-            $query = "SELECT * FROM ENROLLMENT;";
-            /*$query = "SELECT 'Classroom', 'Meeting_Days', 'Beg_Time', 'End_Time', 'Course_Title',
-            FROM COURSE_SECTION as CS, COURSE as C
-            WHERE CS.P_SSN = '.$SSN.' AND CS.Course_Num = C.Course_Num ;";*/
-
-           // $result = $link->query($query);
-
-            if ($result = $link->query($query)) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<table border = '1'>";
+        if ($result = $link->query($query)) {
+            while($row = $result->fetch_assoc()) {
+                echo "<table border = '1'>";
                     // Headings
                     echo"<tr>";
                     echo "<th>Students </th>";
@@ -30,19 +27,17 @@
 
                     //Details
                     echo"<tr>";
-                    echo"<td> </td>";
+                    echo "<td>".$row["COUNT(*)"]."</td>";
                     echo "<td>".$row["Student_Grade"]."</td>";
                     echo "</table>";
-                }
             }
-
-            else {
-                echo "0 results";
-            }
-            
-            $result->free_result();
-            $link->close();
-            }
+        }
+        else {
+            echo "0 results";
+        }
+        
+        $result->free_result();
+        $link->close();
 
         ?>
     </body>
