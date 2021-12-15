68d1c3336d53f9a8b738569d197bedbf980c6805
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-14 23:15:16 -0800

    need to fix query but format is good

--- student_grade.php
+++ student_grade.php
@@ -1,3 +1,49 @@
-<?php
+<html>
+    <body>
+        <?php
+        
+        $link = mysqli_connect('mariadb', 'cs332t14', '6nXl2UmX','cs332t14');
+        if (!$link) {
+            die('Could not connect: ' . mysqli_error());
+        }
 
-?>
+        else {
+            echo "Connected Successfully.<br><br>";
+        }
+
+        if(isset($_POST['CWID'])) {
+            $query = "SELECT * FROM ENROLLMENT;";
+            /*$query = "SELECT 'Classroom', 'Meeting_Days', 'Beg_Time', 'End_Time', 'Course_Title',
+            FROM COURSE_SECTION as CS, COURSE as C
+            WHERE CS.P_SSN = '.$SSN.' AND CS.Course_Num = C.Course_Num ;";*/
+
+           // $result = $link->query($query);
+
+            if ($result = $link->query($query)) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<table border = '1'>";
+                    // Headings
+                    echo"<tr>";
+                    echo "<th>Courses </th>";
+                    echo "<th>Grades </th>";
+                    echo"</tr>";
+
+                    //Details
+                    echo"<tr>";
+                    echo "<td>".$row["Course_Num"]."</td>";
+                    echo "<td>".$row["Student_Grade"]."</td>";
+                    echo "</table>";
+                }
+            }
+
+            else {
+                echo "0 results";
+            }
+            
+            $result->free_result();
+            $link->close();
+            }
+
+        ?>
+    </body>
+</html>
