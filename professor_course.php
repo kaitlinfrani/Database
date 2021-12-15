0cf7b2a7455783aa7a335c4aa0ce54cfba6446f8
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-14 23:13:36 -0800

    Need to fix query, but format is good

--- professor_course.php
+++ professor_course.php
@@ -8,28 +8,49 @@
         }
 
         else {
-            echo "Connected Successfully.";
+            echo "Connected Successfully.<br><br>";
         }
 
         if(isset($_POST['SSN'])) {
-            $query = "SELECT * FROM COURSE_SECTION;";
+            //$query = "SELECT * FROM COURSE_SECTION WHERE P_SSN = '123456789';";
+            $query = "SELECT Classroom, Meeting_Days, Beg_Time, End_Time, Course_Title
+            FROM COURSE_SECTION as CS, COURSE as C
+            WHERE CS.P_SSN = '".$SSN."' AND CS.Course_Num = C.Course_Num;";
+
+           // $result = $link->query($query);
 
             if ($result = $link->query($query)) {
-                // output data of each row
-                echo "Hi";
-                echo "SSN - " .$SSN. "<br>";
                 while($row = $result->fetch_assoc()) {
-                    echo "Title: " . $row["Course_Title"]. " - Classroom: " .$row["Classroom"]. " - Meeting days: " . $row["Meeting_Days"]. " - Start time: " . $row["Beg_Time"]. " - End time: " . $row["End_Time"].  "<br>";
+                    echo "<table border = '1'>";
+                    // Headings
+                    echo"<tr>";
+                    echo "<th>Title </th>";
+                    echo "<th>Classroom </th>";
+                    echo "<th>Meeting Days </th>";
+                    echo "<th>Beginning Time </th>";
+                    echo "<th>End Time </th>";
+                    echo"</tr>";
+
+                    //Details
+                    echo"<tr>";
+                    echo "<td>".$row["Course_Num"]."</td>";
+                    echo "<td>".$row["Classroom"]."</td>";
+                    echo "<td>".$row["Meeting_Days"]."</td>";
+                    echo "<td>".$row["Beg_Time"]."</td>";
+                    echo "<td>".$row["End_Time"]."</td>";
+                    echo "</table>";
                 }
             }
+
             else {
                 echo "0 results";
             }
             
+            //$result->free_result();
+            //$link->close();
+            }
             $result->free_result();
             $link->close();
-            }
-
         ?>
     </body>
 </html>
