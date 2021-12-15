4c9155459436ba8f8dec4afeb038f5b2e852066e
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-15 12:13:20 -0800

    Student's Course Grades
    lists student's course grades when CWID is inputted

--- student_grade.php
+++ student_grade.php
@@ -8,19 +8,24 @@
 
         $CWID = $_POST["CWID"];
         
-         $query = "SELECT Student_CWID, Course_Num, Student_Grade
-         FROM ENROLLMENT
-         WHERE Student_CWID = '".$CWID."';";
+        //Output courses and grades of the student
+        $query = "SELECT Student_CWID, Course_Num, Student_Grade
+        FROM ENROLLMENT
+        WHERE Student_CWID = '".$CWID."';";
         
         $result = $link->query($query);
 
-        if ($result = $link->query($query)) {
+        //Does a check to make sure the data inputted is correct, if not, it will output "0 results"
+        if ($result->num_rows > 0) {
+            echo "<h1>Courses and Grades of Student</h1>";
+            echo "CWID Inputted: ".$CWID;
+            echo "<p></p>";
             while($row = $result->fetch_assoc()) {
                 echo "<table border = '1'>";
                 // Headings
                 echo"<tr>";
-                echo "<th>Courses </th>";
-                echo "<th>Grades </th>";
+                echo "<th>Course </th>";
+                echo "<th>Grade </th>";
                 echo"</tr>";
 
                 //Details
@@ -31,7 +36,13 @@
             }
         }
         else {
-            echo "0 results";
+            echo "<h2>0 results</h2>";
+            echo "<p></p>";
+            echo "Please try again by going back to the main page.";
+            echo "<p></p>";
+            echo "<form action='http://ecs.fullerton.edu/~cs332t14'>";
+            echo "<button type='submit' value='MAIN PAGE'>MAIN PAGE</button>";
+            echo "</form>";
         }
         
         $result->free_result();
