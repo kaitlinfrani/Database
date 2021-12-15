764394f7da796783bd90fca4d9c93043c3c32172
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-12-15 12:11:44 -0800

    Professor's Students' Grades
    lists students' grades of the professor's course and section inputted

--- professor_grade.php
+++ professor_grade.php
@@ -9,31 +9,44 @@
         $cnum = $_POST["coursenum"];
         $snum = $_POST["sectionnum"];
         
+        //Output number of students with certain grade
         $query = "SELECT Student_Grade, COUNT(*)
         FROM COURSE_SECTION as s, COURSE as c, ENROLLMENT as e
-        WHERE c.Course_Num = '".$cnum."' AND s.Section_Num = '".$sum."' AND c.Course_Num = s.Course_Num AND s.Course_Num = e.Course_Num
-        AND s.Section_Num = e.Section_Num;";
+        WHERE c.Course_Num = '".$cnum."' AND s.Section_Num = '".$snum."' AND 
+        c.Course_Num = s.Course_Num AND s.Course_Num = e.Course_Num AND e.Section_Num = s.Section_Num
+        GROUP BY e.Student_Grade;";
         
         $result = $link->query($query);
 
-        if ($result = $link->query($query)) {
+        //Does a check to make sure the data inputted is correct, if not, it will output "0 results"
+        if ($result->num_rows > 0) {
+            echo "<h1>Number of Students with Grade</h1>";
+            echo "Course Number Inputted: ".$cnum."  Section Number Inputted: ".$snum;
+            echo "<p></p>";
             while($row = $result->fetch_assoc()) {
+                echo "<br>";
                 echo "<table border = '1'>";
-                    // Headings
-                    echo"<tr>";
-                    echo "<th>Students </th>";
-                    echo "<th>Grade </th>";
-                    echo"</tr>";
+                // Headings
+                echo"<tr>";
+                echo "<th>Number of Students </th>";
+                echo "<th>Grade </th>";
+                echo"</tr>";
 
-                    //Details
-                    echo"<tr>";
-                    echo "<td>".$row["COUNT(*)"]."</td>";
-                    echo "<td>".$row["Student_Grade"]."</td>";
-                    echo "</table>";
+                //Details
+                echo"<tr>";
+                echo "<td>".$row["COUNT(*)"]."</td>";
+                echo "<td>".$row["Student_Grade"]."</td>";
+                echo "</table>";
             }
         }
         else {
-            echo "0 results";
+            echo "<h2>0 results</h2>";
+            echo "<p></p>";
+            echo "Please try again by going back to the main page.";
+            echo "<p></p>";
+            echo "<form action='http://ecs.fullerton.edu/~cs332t14'>";
+            echo "<button type='submit' value='MAIN PAGE'>MAIN PAGE</button>";
+            echo "</form>";
         }
         
         $result->free_result();
